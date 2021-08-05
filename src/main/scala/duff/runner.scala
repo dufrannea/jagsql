461538b71351d6b150f3159dbd7c1e9224caf5a8
Arnaud Dufranne <a.dufranne@criteo.com>
2021-08-05 16:51:08 +0200

    Create stdin topic only when needed

--- src/main/scala/duff/runner.scala
+++ src/main/scala/duff/runner.scala
@@ -2,17 +2,22 @@ package duff.jagsql
 package runner
 
 import cats._
+import cats.data.Binested
+import cats.data.EitherK
+import cats.data.Nested
 import cats.effect._
+import cats.implicits._
 import duff.jagsql.std.PersistentTopic
 import fs2.Stream
 import fs2.io._
 import fs2.text
 
+import java.io.FileInputStream
+
 import ast.Expression
 import ast.Projection
 import eval._
 import planner._
-import java.io.FileInputStream
 
 case class Row(colValues: List[(String, Value)])
 
@@ -36,71 +41,101 @@ def toStream(s: Stage, stdInLines: Stream[IO, String] = defaultStdInLines): fs2.
         topic.publish
       )
 
-  def toStream0(s: Stage, topic: PersistentTopic[IO, Option[Row]], name: Option[String] = None): fs2.Stream[IO, Row] =
+  def toStream0(s: Stage, name: Option[String] = None): MaybeNeedsTopic[Stream[IO, Row]] =
     s match {
       case Stage.ReadStdIn                                     =>
-        topic
-          .subscribe(10)
-          .evalMap(k =>
-            IO {
-              k
-            }
-          )
-          .unNoneTerminate
+        Left(topic =>
+          topic
+            .subscribe(10)
+            .unNoneTerminate
+        )
       case Stage.ReadFile(file)                                =>
-        readInputStream[IO](IO(new FileInputStream(file)), 1024)
-          .through(fs2.text.utf8Decode)
-          .through(fs2.text.lines)
-          .dropLastIf(_.isEmpty)
-          .map(line => Row(List(("col_0", Value.VString(line)))))
+        Right(
+          readInputStream[IO](IO(new FileInputStream(file)), 1024)
+            .through(fs2.text.utf8Decode)
+            .through(fs2.text.lines)
+            .dropLastIf(_.isEmpty)
+            .map(line => Row(List(("col_0", Value.VString(line)))))
+        )
       case Stage.Projection(projections, source)               =>
-        val sourceStream = toStream0(source, topic)
-        sourceStream.map { case Row(cols) =>
+        val sourceStream = toStream0(source)
+
+        // Need to explicit the types as we do not want
+        // the function instance of Either to be picked up
+        Nested[MaybeNeedsTopic, [X] =>> Stream[IO, X], Row](sourceStream).map { case Row(cols) =>
           val lookup = cols.toMap
           val colValues = projections
             .zipWithIndex
             .map { case (Projection(expression, maybeAlias), index) =>
               (maybeAlias.getOrElse(s"col_$index"), eval(expression)(lookup))
             }
+
           Row(colValues.toList)
-        }
+        }.value
       case Stage.Filter(predicate, source)                     =>
-        val sourceStream = toStream0(source, topic)
-        sourceStream.filter { case Row(inputColValues) =>
+        val sourceStream = toStream0(source)
+
+        sourceStream.map(_.filter { case Row(inputColValues) =>
           val lookup = inputColValues.toMap
           unsafeEvalBool(predicate)(lookup)
-        }
+        })
       case Stage.Join(leftSource, rightSource, maybePredicate) =>
-        val leftStream = toStream0(leftSource, topic, Some("left"))
-        val rightStream = toStream0(rightSource, topic, Some("right"))
+        val leftStream = toStream0(leftSource, Some("left"))
+        val rightStream = toStream0(rightSource, Some("right"))
 
         // one option would be to fully store the right stream
         // in memory, and reiterate on it. Doing so would prevent
         // from displaying values as soon as possible, so an alternative
         // approach using a PersistentTopic is used here.
-        val crossJoin = for {
-          leftRow  <- leftStream
-          rightRow <- rightStream
-          allCols = leftRow.colValues ++ rightRow.colValues
-        } yield Row(allCols)
+        (leftStream, rightStream).mapN { case (ls, rs) =>
+          val crossJoin = for {
+            leftRow  <- ls
+            rightRow <- rs
+            allCols = leftRow.colValues ++ rightRow.colValues
+          } yield Row(allCols)
 
-        maybePredicate match {
-          case None            => crossJoin
-          case Some(predicate) =>
-            val filtered = crossJoin.filter { case Row(colValues) =>
-              val values = colValues.toMap
-              unsafeEvalBool(predicate)(values)
-            }
-            filtered
+          maybePredicate match {
+            case None            => crossJoin
+            case Some(predicate) =>
+              val filtered = crossJoin.filter { case Row(colValues) =>
+                val values = colValues.toMap
+                unsafeEvalBool(predicate)(values)
+              }
+              filtered
+          }
         }
     }
 
-  val result = for {
-    topic  <- Stream.eval(PersistentTopic[IO, Option[Row]])
-    stream <- publishStdIn(topic)
-                .drain
-                .mergeHaltR(toStream0(s, topic))
-  } yield stream
+  toStream0(s) match {
+    case Left(needsTopic)     =>
+      for {
+        topic  <- Stream.eval(PersistentTopic[IO, Option[Row]])
+        stream <- publishStdIn(topic)
+                    .drain
+                    .mergeHaltR(needsTopic(topic))
+      } yield stream
+    case Right(noTopicNeeded) =>
+      noTopicNeeded
+  }
+}
 
-  result
+type StdInTopic = PersistentTopic[IO, Option[Row]]
+
+type MaybeNeedsTopic[K] = Either[StdInTopic => K, Id[K]]
+
+given maybeNeedsTopicApply: Apply[MaybeNeedsTopic] = new MaybeNeedsTopicApply {}
+
+trait MaybeNeedsTopicApply extends Apply[MaybeNeedsTopic] {
+  override def map[A, B](fa: MaybeNeedsTopic[A])(f: A => B) =
+    EitherK(fa).map(f).run
+
+  override def ap[A, B](f: MaybeNeedsTopic[A => B])(fa: MaybeNeedsTopic[A]): MaybeNeedsTopic[B] = {
+    (f, fa) match {
+      case (Right(ff), Right(a)) => Right(ff(a))
+      case (Left(ff), Right(a))  => Left(topic => ff(topic)(a))
+      case (Right(ff), Left(a))  => Left(topic => ff(a(topic)))
+      case (Left(ff), Left(a))   => Left(topic => ff(topic)(a(topic)))
+    }
+  }
+
 }
